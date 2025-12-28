7064b7fe9d491fa6fff9639b4f1735037c79c5d6
Annie Huang <annie.hn@gmail.com>
2025-12-28 21:34:50 +1100

    Call the createTicket method in the form action in app/tickets/new/page.tsx and tested that we can get the form info in terminal console.

--- actions/ticket.actions.ts
+++ actions/ticket.actions.ts
@@ -5,6 +5,7 @@ export async function createTicket(formData: FormData) {
   const description = formData.get('description') as string;
   const priority = formData.get('priority') as string;
 
+  // The log is show in terminal for the server, not in the devtool console.
   console.log(subject, description, priority);
 
   return { success: true, message: 'Ticket created successfully' };
