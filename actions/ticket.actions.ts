e14e48a5ce6c2c31b2bc52e77319571129b91d7c
Annie Huang <annie.hn@gmail.com>
2025-12-29 19:39:14 +1100

    Move the method content into a try catch block for createTicket

--- actions/ticket.actions.ts
+++ actions/ticket.actions.ts
@@ -6,17 +6,22 @@ export async function createTicket(
   prevState: { success: boolean; message: string }, // Need to create prevState otherwise useActionState(createTicket will get error.
   formData: FormData,
 ): Promise<{ success: boolean; message: string }> {
-  const subject = formData.get('subject') as string;
-  const description = formData.get('description') as string;
-  const priority = formData.get('priority') as string;
+  try {
+    const subject = formData.get('subject') as string;
+    const description = formData.get('description') as string;
+    const priority = formData.get('priority') as string;
 
-  // The log is show in terminal for the server, not in the devtool console.
-  console.log(subject, description, priority);
+    // The log is show in terminal for the server, not in the devtool console.
+    console.log(subject, description, priority);
 
-  if (!subject || !description || !priority) {
-    Sentry.captureMessage('Validation Error: Missing ticket fields');
-    return { success: false, message: 'All fields are required' };
-  }
+    if (!subject || !description || !priority) {
+      Sentry.captureMessage(
+        'Validation Error: Missing ticket fields',
+        'warning',
+      );
+      return { success: false, message: 'All fields are required' };
+    }
 
-  return { success: true, message: 'Ticket created successfully' };
+    return { success: true, message: 'Ticket created successfully' };
+  } catch (error) {}
 }
