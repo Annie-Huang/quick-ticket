343e086c1c7d029e315ad1e4ca49f2f21b860d3e
Annie Huang <annie.hn@gmail.com>
2025-12-29 20:03:59 +1100

    Add notes

--- db/prisma.ts
+++ db/prisma.ts
@@ -1,3 +1,11 @@
+// Note: we need to create this special file for our Prisma client.
+// Because if we use the prisma client as it is, we could run into some issues because of the way
+// that server side rendering works and the way vercel work with Neon, which is serverless, meaning it's not like
+// a continues connection. So we don't want a case where multiple Prisma instances are being created. Basically we
+// want to create a global instance and then reuse that unless it's not created. So we want to check a global prisma instance.
+// If there is, use it. If there's not, then create it.
+
+// Get the PrismaClient from the generated file rather than create it every time.
 // import { PrismaClient } from '@/generated/prisma';
 import { PrismaClient } from '../app/generated/prisma';
 
