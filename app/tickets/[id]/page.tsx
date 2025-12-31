2e3e05d79d7103f6de1d26be4df9bd844a83a370
Annie Huang <annie.hn@gmail.com>
2025-12-31 20:37:36 +1100

    Call getTicketById from ticket.action.ts. If not found, call the notFound method from next/navigation

--- app/tickets/[id]/page.tsx
+++ app/tickets/[id]/page.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { getTicketById } from '@/actions/ticket.actions';
+import { notFound } from 'next/navigation';
 
 const TicketDetailsPage = async (props: {
   params: Promise<{ id: string }>;
 }) => {
   const { id } = await props.params;
+
+  const ticket = await getTicketById(id);
+  if (!ticket) {
+    // Actually surprised we don't need to call 'return' on this method.
+    notFound();
+  }
 
   return <>details {id}</>;
 };
