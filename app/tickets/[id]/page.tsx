59e195e48f9a5b6c53b49e8ca456bbdc6670414c
Annie Huang <annie.hn@gmail.com>
2025-12-31 20:39:46 +1100

    Log sentry event when we are able to find the ticket.

--- app/tickets/[id]/page.tsx
+++ app/tickets/[id]/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { getTicketById } from '@/actions/ticket.actions';
 import { notFound } from 'next/navigation';
+import { logEvent } from '@/utils/sentry';
 
 const TicketDetailsPage = async (props: {
   params: Promise<{ id: string }>;
@@ -12,6 +13,8 @@ const TicketDetailsPage = async (props: {
     // Actually surprised we don't need to call 'return' on this method.
     notFound();
   }
+
+  logEvent('View ticket details', 'ticket', { ticketId: ticket.id }, 'info');
 
   return <>details {id}</>;
 };
