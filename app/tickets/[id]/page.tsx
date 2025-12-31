5ce6ca4afc38f09b9d61642880baa09fa5919179
Annie Huang <annie.hn@gmail.com>
2025-12-31 20:35:06 +1100

    Get the params from url and prove we can get the correct value.

--- app/tickets/[id]/page.tsx
+++ app/tickets/[id]/page.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
-const TicketDetailsPage = () => {
-  return <>details</>;
+const TicketDetailsPage = async (props: {
+  params: Promise<{ id: string }>;
+}) => {
+  const { id } = await props.params;
+
+  return <>details {id}</>;
 };
 
 export default TicketDetailsPage;
