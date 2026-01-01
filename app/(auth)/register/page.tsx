b9dc09654c577ed61f23506a0632fd592c55db94
Annie Huang <annie.hn@gmail.com>
2026-01-01 20:15:32 +1100

    Call useActionState for the registerUser in register/page.tsx

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,6 +1,16 @@
-import React from 'react';
+'use client';
+
+import { useActionState } from 'react';
+import { registerUser } from '@/actions/auth.actions';
 
 const RegisterPage = () => {
+  const initialState = {
+    success: false,
+    message: '',
+  };
+
+  const [state, formAction] = useActionState(registerUser, initialState);
+
   return <>Register</>;
 };
 
