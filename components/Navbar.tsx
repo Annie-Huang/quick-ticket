a11149e1e7178955df0d221b65fed7f7211da217
Annie Huang <annie.hn@gmail.com>
2026-01-01 21:21:21 +1100

    The submit button need to be inside a form.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -27,12 +27,14 @@ const Navbar = async () => {
             >
               My Tickets
             </Link>
-            <button
-              type='submit'
-              className='bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 transition'
-            >
-              Logout
-            </button>
+            <form>
+              <button
+                type='submit'
+                className='bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 transition'
+              >
+                Logout
+              </button>
+            </form>
           </>
         ) : (
           <>
