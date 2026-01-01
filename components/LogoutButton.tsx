e4efb15432c8c5e072ecdf1dfde33e76aae863b9
Annie Huang <annie.hn@gmail.com>
2026-01-02 09:07:05 +1100

    Create useActionState and link the formAction to the <form> component.

--- components/LogoutButton.tsx
+++ components/LogoutButton.tsx
@@ -1,8 +1,20 @@
-import React from 'react';
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { useActionState } from 'react';
+import { logoutUser } from '@/actions/auth.actions';
 
 const LogoutButton = () => {
+  const router = useRouter();
+  const initialState = {
+    success: false,
+    message: '',
+  };
+
+  const [state, formAction] = useActionState(logoutUser, initialState);
+
   return (
-    <form>
+    <form action={formAction}>
       <button
         type='submit'
         className='bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 transition'
