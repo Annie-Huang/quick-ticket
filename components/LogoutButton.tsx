08345eff9f20bfe97b8c72838ff1d4e02c506695
Annie Huang <annie.hn@gmail.com>
2026-01-02 09:01:40 +1100

    Create LogoutButton.tsx component and move the button from Navbar to there.

---
+++ components/LogoutButton.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const LogoutButton = () => {
+  return (
+    <form>
+      <button
+        type='submit'
+        className='bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 transition'
+      >
+        Logout
+      </button>
+    </form>
+  );
+};
+
+export default LogoutButton;
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import Link from 'next/link';
 import { getCurrentUser } from '@/lib/current-user';
+import LogoutButton from '@/components/LogoutButton';
 
+// Don't want to make Navbar a client component so abstract the logout button into its own component.
 const Navbar = async () => {
   const user = await getCurrentUser();
 
@@ -27,14 +29,7 @@ const Navbar = async () => {
             >
               My Tickets
             </Link>
-            <form>
-              <button
-                type='submit'
-                className='bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 transition'
-              >
-                Logout
-              </button>
-            </form>
+            <LogoutButton />
           </>
         ) : (
           <>
